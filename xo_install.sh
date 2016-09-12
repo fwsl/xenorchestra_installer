d80556cf84b53c5010472745ba6e6a511da69803
mac <grzesiekz@gmail.com>
2016-09-12 13:37:02 +0200

    cleanup

--- xo_install.sh
+++ xo_install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install --yes nfs-common git curl npm
+sudo apt-get install --yes nfs-common git curl npm build-essential redis-server libpng12-dev git python-minimal
 cd /opt
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install --yes nodejs
 curl -o /usr/local/bin/n https://raw.githubusercontent.com/visionmedia/n/master/bin/n
 sudo chmod +x /usr/local/bin/n
 sudo n stable
-sudo apt-get install --yes build-essential redis-server libpng12-dev git python-minimal
 git clone -b stable https://github.com/vatesfr/xo-server
 git clone -b stable https://github.com/vatesfr/xo-web
 cd xo-server
