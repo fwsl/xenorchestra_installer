80d734e5661cca57fed2744c7e3f5a519d26b1d9
mac <grzesiekz@gmail.com>
2016-09-13 11:31:27 +0200

    script can now upgrade existing installation, creates tar.gz backups

--- xo_install.sh
+++ xo_install.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install --yes nfs-common git curl npm build-essential redis-server libpng12-dev git python-minimal
-cd /opt
+WORKDIR=/opt
+xowebDIR="$WORKDIR"/xo-web
+xoserverDIR="$WORKDIR"/xo-server
+
+
+if [ -d "$xowebDIR" ]; then
+    printf '%s\n' "Creating backup of ($xowebDIR)"
+    tar -zcf "$WORKDIR"/xo-web-$(date +%Y-%m-%d-%X).tar.gz "$xowebDIR"
+    printf '%s\n' "Removing old ($xowebDIR)"
+    rm -rf "$xowebDIR"
+fi
+if [ -d "$xoserverDIR" ]; then
+    printf '%s\n' "Creating backup of ($xoserverDIR)"
+    tar -zcf "$WORKDIR"/xo-server-$(date +%Y-%m-%d-%X).tar.gz "$xoserverDIR"
+    printf '%s\n' "Removing old ($xoserverDIR)"
+    rm -rf "$xoserverDIR"
+fi
+
+apt-get update
+apt-get install --yes nfs-common git curl npm build-essential redis-server libpng12-dev git python-minimal
+cd "$WORKDIR"
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install --yes nodejs
+apt-get install --yes nodejs
 curl -o /usr/local/bin/n https://raw.githubusercontent.com/visionmedia/n/master/bin/n
-sudo chmod +x /usr/local/bin/n
-sudo n stable
+chmod +x /usr/local/bin/n
+n stable
 git clone -b stable https://github.com/vatesfr/xo-server
 git clone -b stable https://github.com/vatesfr/xo-web
-cd xo-server
-sudo npm install && npm run build
-sudo cp sample.config.yaml .xo-server.yaml
-sudo sed -i /mounts/a\\"    '/': '/opt/xo-web/dist'" .xo-server.yaml
-sudo npm install xo-server-transport-email
-sudo npm install xo-server-backup-reports
-cd /opt/xo-web
-sudo npm install
-sudo npm run build
+cd "$xoserverDIR"
+npm install && npm run build
+cp sample.config.yaml .xo-server.yaml
+sed -i /mounts/a\\"    '/': '/opt/xo-web/dist'" .xo-server.yaml
+npm install xo-server-transport-email
+npm install xo-server-backup-reports
+cd "$xowebDIR"
+npm install
+npm run build
 cat > /etc/systemd/system/xo-server.service <<EOF
 # systemd service for XO-Server.
 
@@ -35,6 +53,6 @@ SyslogIdentifier=xo-server
 WantedBy=multi-user.target
 EOF
 
-sudo chmod +x /etc/systemd/system/xo-server.service
-sudo systemctl enable xo-server.service
-sudo systemctl start xo-server.service
+chmod +x /etc/systemd/system/xo-server.service
+systemctl enable xo-server.service
+systemctl start xo-server.service
